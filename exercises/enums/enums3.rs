a47063a9f18e40b44912e12c41af7e7e7819d448
lilpumppp22 <2714920252@qq.com>
2024-04-21 21:13:14 +0800

    111

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,9 +5,13 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM  DONE
 
 enum Message {
+    Move (Point),
+    Echo (String),
+    ChangeColor(u8,u8,u8),
+    Quit ,
     // TODO: implement the message variant types based on their usage below
 }
 
@@ -39,12 +43,24 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match message{
+            Message::Move(Point{x,y})=>self.move_position(Point{x:10,y:15}),
+            Message::Echo(s)=>self.echo(s),
+       Message::ChangeColor(r,g,b) => 
+        self.change_color((255,0,255)),
+       
+       
+       Message::Quit =>self.quit=true,
+      
+
+       }
+    }
         // TODO: create a match expression to process the different message
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
     }
-}
+
 
 #[cfg(test)]
 mod tests {
--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM  DONE
 
 #[derive(Debug)]
 struct Package {
@@ -29,11 +29,18 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
+        if self.sender_country!=self.recipient_country{
+            return true
+        }else{
+            return false
+        }
         // Something goes here...
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        cents_per_gram * self.weight_in_grams
+        
         // Something goes here...
     }
 }
